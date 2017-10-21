23cde84e06b5bc83c7ebd6e66197e9d8d501df48
Colin <c.walsh8600@gmail.com>
2017-10-21 01:32:09 -0400

    Updating

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, 'public/index-contact.html'));
+    res.sendFile(path.join(__dirname, 'public/register.html'));
 });
 
 app.use(function (err, req, res, next) {
