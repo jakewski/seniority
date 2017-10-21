8b67551eadbc3a6d756c0e108c63b8bb3ab712fe
Jakub <jakewski@gmail.com>
2017-10-20 21:48:46 -0400

    routes skelly

---
+++ server/routes/login.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const db = require('../../models');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+
+})
+
+module.exports = router;
